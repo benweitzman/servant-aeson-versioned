d0b3de72fe9c664b03f286e51c90b8d7a2ea3e05
Ben Weitzman <benweitzman@gmail.com>
2016-11-10 19:00:00 -0500

    some tests for FailableToJSON instances

--- test/Servant/API/ContentTypes/AesonVersionedSpec.hs
+++ test/Servant/API/ContentTypes/AesonVersionedSpec.hs
@@ -30,8 +30,8 @@ import Test.Hspec.Wai
 import Servant.API.ContentTypes.AesonVersioned
 
 type JSONVersionApi =
-         "foo" :> Get '[JSONVersioned V1, JSONVersioned V2] Person
-    :<|> "foos" :> Get '[JSONVersioned V1, JSONVersioned V2] [Person]
+         "foo" :> Get '[JSONVersioned V1, JSONVersioned V2, JSONVersioned V3] Person
+    :<|> "foos" :> Get '[JSONVersioned V1, JSONVersioned V2, JSONVersioned V3] [Person]
 
 jsonVersionApi :: Proxy JSONVersionApi
 jsonVersionApi = Proxy
@@ -49,6 +49,18 @@ spec = do
       it "should serialize v2" $ do
         response <- request methodGet "/foo" [("Accept", "application/json;version=2.0")] ""
         liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON (Tagged alice :: Tagged V2 Person))
+      it "should fail to serialize v3" $ do
+        request methodGet "/foo" [("Accept", "application/json;version=3.0")] "" `shouldRespondWith` 406
+      it "should serialize [v1]" $ do
+        response <- request methodGet "/foos" [("Accept", "application/json;version=1.0")] ""
+        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON [toJSON (Tagged alice :: Tagged V1 Person)])
+      it "should serialize [v2]" $ do
+        response <- request methodGet "/foos" [("Accept", "application/json;version=2.0")] ""
+        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON [toJSON (Tagged alice :: Tagged V2 Person)])
+      it "should serialize empty [v3]" $ do
+        response <- request methodGet "/foos" [("Accept", "application/json;version=3.0")] ""
+        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON ([] :: [Int]))
+
 
 
 
@@ -69,4 +81,12 @@ instance ToJSON (Tagged V1 Person) where
 
 instance ToJSON (Tagged V2 Person) where
     toJSON (Tagged (Person name age)) = object [ "name" .= name
-                                               , "age" .= age]
+                                               , "age" .= age
+                                               ]
+
+instance FailableToJSON (Tagged V3 Person) where
+  mToJSON (Tagged (Person name age))
+    | age >= 25 = Nothing
+    | otherwise = Just $ object [ "name" .= name
+                                , "age" .= age
+                                ]
