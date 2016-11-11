4fcc5263f1846334ba22b8896ec20661c70b02a5
Ben Weitzman <benweitzman@gmail.com>
2016-11-11 15:32:41 -0500

    deserializing and more tests

--- src/Servant/API/ContentTypes/AesonVersioned.hs
+++ src/Servant/API/ContentTypes/AesonVersioned.hs
@@ -8,39 +8,44 @@
 {-# LANGUAGE EmptyCase #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Servant.API.ContentTypes.AesonVersioned where
 
-import Servant.API.ContentTypes
+import Servant.API
 
-import Control.Applicative
-
-import Control.Error
-
-import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Char8 as BS
-
-import Data.CaseInsensitive
 
 import Data.Aeson
 import Data.Aeson.Versions
 
-import Data.Functor.Identity
-import Data.Functor.Compose
-
-import Data.Maybe
-import Data.Monoid
-
 import Data.Proxy
 import Data.Tagged
 
+import Data.Functor.Identity
+
+import Control.Error
+
 import GHC.TypeLits
 
-import Text.Read
-
-import qualified Data.Map as M
-
 import Network.HTTP.Media hiding (Accept)
+
+type family Map (f :: k -> k') (xs :: [k]) :: [k'] where
+  Map f '[] = '[]
+  Map f (x ': xs) = f x ': Map f xs
+
+type family Append xs ys where
+    Append '[] ys = ys
+    Append (x ': xs) ys = x ': Append xs ys
+
+
+type family GetV rest a where
+    GetV rest (f a) = Get (Append (Map JSONVersioned (SerializerVersions a)) rest) (f a)
+    GetV rest a = Get (Append (Map JSONVersioned (SerializerVersions a)) rest) a
+
+type family ReqBodyV rest a where
+    ReqBodyV rest (f a) = ReqBody (Append (Map JSONVersioned (DeserializerVersions a)) rest) (f a)
+    ReqBodyV rest a = ReqBody (Append (Map JSONVersioned (DeserializerVersions a)) rest) a
 
 data JSONVersioned (v :: Version Nat Nat)
 
@@ -60,120 +65,19 @@ instance {-# OVERLAPPING #-}
                serializer = snd $ getSerializer (Proxy :: Proxy v)
 
 
-{-
-instance {-# OVERLAPPING #-} (KnownVersion v, AllMimeRender cs a, FailableToJSON (Tagged v a)) => AllMimeRender ((JSONVersioned v) ': cs) a where
-    allMimeRender _ val = case mToJSON (Tagged val :: Tagged v a) of
-      Just json -> (contentType (Proxy :: Proxy (JSONVersioned v)), encode json) : (allMimeRender (Proxy :: Proxy cs) val)
-      Nothing -> allMimeRender (Proxy :: Proxy cs) val
--}
+instance {-# OVERLAPPABLE #-} (FromJSON (Tagged v a), KnownVersion v) => MimeUnrender (JSONVersioned v) a where
+  mimeUnrender _ body = do
+    val <- eitherDecode body
+    a <- note "Invalid json version" $ runDeserializer deserializer val
+    return $ runIdentity a
 
-{-
-instance {-# OVERLAPPING #-} (KnownVersion v, AllMimeRender cs (f a), FailableToJSON (Tagged v a)
-         ,CatMaybes f, FunctorToJSON f)
-    => AllMimeRender ((JSONVersioned v) ': cs) (f a) where
-    allMimeRender _ val = case runSerializer serializer val of
-      Just json -> (contentType (Proxy :: Proxy (JSONVersioned v)), encode json) : allMimeRender (Proxy :: Proxy cs) val
-      Nothing -> allMimeRender (Proxy :: Proxy cs) val
+    where  deserializer :: Deserializer a
+           deserializer = snd $ getDeserializer (Proxy :: Proxy v)
 
-      where  serializer :: Serializer a
-             serializer = snd $ getSerializer (Proxy :: Proxy v)
--}
+instance {-# OVERLAPPING #-} (TraversableFromJSON t, FromJSON (Tagged v a), KnownVersion v) => MimeUnrender (JSONVersioned v) (t a) where
+  mimeUnrender _ body = do
+    val <- eitherDecode body
+    note "Invalid json version" $ runDeserializer deserializer val
 
-{-
-noParams :: MediaType -> MediaType
-noParams media = original (mainType media) // original (subType media)
-
-refineAccept :: MediaType -- ^ default
-             -> MediaType -- ^ input
-             -> Maybe MediaType
-refineAccept defaultMedia media
-  | ("application" // "json") `matches` noParams media = Just $ case media /. "version" of
-    Just v -> ("application" // "json") /: ("version", original v)
-    Nothing -> defaultMedia
-  | otherwise = Nothing
-
-getAcceptMedia :: BS.ByteString -> MediaType -> Maybe MediaType
-getAcceptMedia acceptHeader defaultMedia = case sequence $ parseAccept <$> BS.split ',' acceptHeader of
-  Nothing -> Nothing
-  Just medias -> case getFirst . mconcat $ First . refineAccept defaultMedia <$> medias of
-    Just media -> Just media
-    Nothing -> Nothing
-
-
-getContentType :: BS.ByteString -> MediaType -> Maybe MediaType
-getContentType contentTypeHeader defaultMedia = case parseAccept contentTypeHeader of
-  Nothing -> Nothing
-  Just media -> if media `matches` ("application" // "json")
-                then Just $ if media /? "version"
-                            then media
-                            else defaultMedia
-                else Nothing
-
-
-instance {-# OVERLAPPING #-} AllMime list => AllMime (JSONVersioned ': list) where
-  allMime _ = allMime (Proxy :: Proxy list)
-
-
-instance AllCTRender '[] a where
-  handleAcceptH _ _ _ = Nothing
-
-
-instance {-# OVERLAPPING #-} (AllCTRender list String) => AllCTRender (JSONVersioned ': list) String where
-  handleAcceptH p ah@(AcceptHeader acceptHeader) a =
-    handleAcceptH (Proxy :: Proxy '[JSONVersioned]) ah (Identity a)
-    <|>
-    handleAcceptH (Proxy :: Proxy list) ah a
-
-instance {-# OVERLAPPABLE #-} (SerializedVersion a, AllCTRender list a) => AllCTRender (JSONVersioned ': list) a where
-  handleAcceptH p ah@(AcceptHeader acceptHeader) a =
-    handleAcceptH (Proxy :: Proxy '[JSONVersioned]) ah (Identity a)
-    <|>
-    handleAcceptH (Proxy :: Proxy list) ah a
-
-instance (SerializedVersion a, FunctorToJSON f
-         ,CatMaybes f, AllCTRender list (f a)) => AllCTRender (JSONVersioned ': list) (f a) where
-  handleAcceptH _ ah@(AcceptHeader acceptHeader) a = (do
-    let serializers' = serializers
-        defaultVersion = fst $ M.findMax serializers'
-    media <- getAcceptMedia acceptHeader $ "application" // "json" /: ("version", BS.pack $ show defaultVersion)
-    let mVersionBS = original <$> media /. "version"
-    version <- maybe (pure defaultVersion) parseVersion mVersionBS
-    serializer <- M.lookup version serializers'
-    value <- serialize serializer a
-    return $ (encode value, LBS.fromStrict $ renderHeader media))
-    <|>
-    handleAcceptH (Proxy :: Proxy list) ah a
-
-parseVersion :: BS.ByteString -> Maybe (Version Integer Integer)
-parseVersion v = readMaybe (BS.unpack v)
-
-instance {-# OVERLAPPING #-} (AllCTUnrender list String) => AllCTUnrender (JSONVersioned ': list) String where
-  handleCTypeH _ ch reqBody =
-    (getCompose . fmap runIdentity . Compose $ handleCTypeH (Proxy :: Proxy '[JSONVersioned]) ch reqBody)
-    <|>
-    handleCTypeH (Proxy :: Proxy list) ch reqBody
-
-
-instance (DeserializedVersion a
-         ,AllCTUnrender list a) => AllCTUnrender (JSONVersioned ': list) a where
-  handleCTypeH _ ch reqBody =
-    (getCompose . fmap runIdentity . Compose $ handleCTypeH (Proxy :: Proxy '[JSONVersioned]) ch reqBody)
-    <|>
-    handleCTypeH (Proxy :: Proxy list) ch reqBody
-
-instance {-# OVERLAPPING #-} (TraversableFromJSON t, DeserializedVersion a
-         ,AllCTUnrender list (t a)) => AllCTUnrender (JSONVersioned ': list) (t a) where
-  handleCTypeH _ contentTypeHeader reqBody = (do
-    let deserializers' = deserializers
-        defaultVersion = fst $ M.findMax deserializers'
-    media <- getContentType (LBS.toStrict contentTypeHeader) $
-      "application" // "json" /: ("version", BS.pack $ show defaultVersion)
-    let mVersionBS = original <$> media /. "version"
-    version <- maybe (pure defaultVersion) parseVersion mVersionBS
-    deserializer <- M.lookup version deserializers'
-    return $ do
-      value <- eitherDecodeLenient reqBody
-      note "Valid JSON but didn't match version" $ deserialize deserializer value)
-    <|>
-    handleCTypeH (Proxy :: Proxy list) contentTypeHeader reqBody
--}
+    where  deserializer :: Deserializer a
+           deserializer = snd $ getDeserializer (Proxy :: Proxy v)
--- test/Servant/API/ContentTypes/AesonVersionedSpec.hs
+++ test/Servant/API/ContentTypes/AesonVersionedSpec.hs
@@ -5,6 +5,10 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Servant.API.ContentTypes.AesonVersionedSpec where
 
@@ -30,36 +34,67 @@ import Test.Hspec.Wai
 import Servant.API.ContentTypes.AesonVersioned
 
 type JSONVersionApi =
-         "foo" :> Get '[JSONVersioned V1, JSONVersioned V2, JSONVersioned V3] Person
-    :<|> "foos" :> Get '[JSONVersioned V1, JSONVersioned V2, JSONVersioned V3] [Person]
+         "foo" :> GetV '[] Person
+    :<|> "foos" :> GetV '[] [Person]
+    :<|> "bar" :> ReqBodyV '[] Person :> GetV '[] Person
+    :<|> "bars" :> ReqBodyV '[] [Person] :> GetV '[] [Person]
 
 jsonVersionApi :: Proxy JSONVersionApi
 jsonVersionApi = Proxy
 
 jsonVersionServer :: Server JSONVersionApi
-jsonVersionServer = return alice :<|> return [alice]
+jsonVersionServer =
+       return alice
+  :<|> return [alice]
+  :<|> return
+  :<|> return
 
 spec :: Spec
-spec = do
+spec = with (return $ serve jsonVersionApi jsonVersionServer) $ do
   describe "serializing" $ do
-    with (return $ serve jsonVersionApi jsonVersionServer) $ do
-      it "should serialize v1" $ do
-        response <- request methodGet "/foo" [("Accept", "application/json;version=1.0")] ""
-        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON (Tagged alice :: Tagged V1 Person))
-      it "should serialize v2" $ do
-        response <- request methodGet "/foo" [("Accept", "application/json;version=2.0")] ""
-        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON (Tagged alice :: Tagged V2 Person))
-      it "should fail to serialize v3" $ do
-        request methodGet "/foo" [("Accept", "application/json;version=3.0")] "" `shouldRespondWith` 406
-      it "should serialize [v1]" $ do
-        response <- request methodGet "/foos" [("Accept", "application/json;version=1.0")] ""
-        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON [toJSON (Tagged alice :: Tagged V1 Person)])
-      it "should serialize [v2]" $ do
-        response <- request methodGet "/foos" [("Accept", "application/json;version=2.0")] ""
-        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON [toJSON (Tagged alice :: Tagged V2 Person)])
-      it "should serialize empty [v3]" $ do
-        response <- request methodGet "/foos" [("Accept", "application/json;version=3.0")] ""
-        liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON ([] :: [Int]))
+    it "should serialize v1" $ do
+      response <- request methodGet "/foo" [("Accept", "application/json;version=1.0")] ""
+      liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON (Tagged alice :: Tagged V1 Person))
+    it "should serialize v1" $ do
+      response <- request methodGet "/foo" [("Accept", "application/json;version=1.0")] ""
+      liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON (Tagged alice :: Tagged V1 Person))
+    it "should serialize v2" $ do
+      response <- request methodGet "/foo" [("Accept", "application/json;version=2.0")] ""
+      liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON (Tagged alice :: Tagged V2 Person))
+    it "should fail to serialize v3" $ do
+      request methodGet "/foo" [("Accept", "application/json;version=3.0")] "" `shouldRespondWith` 406
+    it "should fail to serialize non existent version" $ do
+      request methodGet "/foo" [("Accept", "application/json;version=4.0")] "" `shouldRespondWith` 406
+    it "should serialize [v1]" $ do
+      response <- request methodGet "/foos" [("Accept", "application/json;version=1.0")] ""
+      liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON [toJSON (Tagged alice :: Tagged V1 Person)])
+    it "should serialize [v2]" $ do
+      response <- request methodGet "/foos" [("Accept", "application/json;version=2.0")] ""
+      liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON [toJSON (Tagged alice :: Tagged V2 Person)])
+    it "should serialize empty [v3]" $ do
+      response <- request methodGet "/foos" [("Accept", "application/json;version=3.0")] ""
+      liftIO $ decode' (simpleBody response) `shouldBe` Just (toJSON ([] :: [Int]))
+  describe "deserializing" $ do
+    it "should deserialize v1" $ do
+      let body = encode (Tagged alice :: Tagged V2 Person)
+      request methodGet "/bar" [("Content-Type", "application/json;version=1.0")] body `shouldRespondWith` 200
+    it "should deserialize v2" $ do
+      let body = encode (Tagged alice :: Tagged V1 Person)
+      request methodGet "/bar" [("Content-Type", "application/json;version=2.0")] body `shouldRespondWith` 200
+    it "should fail to deserialize non existent version" $ do
+      let body = encode (Tagged alice :: Tagged V1 Person)
+      request methodGet "/bar" [("Content-Type", "application/json;version=3.0")] body `shouldRespondWith` 415
+    it "should fail to deserialize bad data" $ do
+      let body = encode (Tagged alice :: Tagged V1 Person)
+      request methodGet "/bar" [("Content-Type", "application/json;version=1.0")] body `shouldRespondWith` 400
+    it "should deserialize [v1]" $ do
+      let body = encode $ toJSON [toJSON (Tagged alice :: Tagged V2 Person)]
+      request methodGet "/bars" [("Content-Type", "application/json;version=1.0")] body `shouldRespondWith` 200
+    it "should deserialize [v2]" $ do
+      let body = encode $ toJSON [toJSON (Tagged alice :: Tagged V1 Person)]
+      request methodGet "/bars" [("Content-Type", "application/json;version=2.0")] body `shouldRespondWith` 200
+
+
 
 
 
@@ -74,7 +109,7 @@ data Person = Person String Int deriving (Generic, Eq)
 alice :: Person
 alice = Person "alice" 25
 
-instance ToJSON Person
+-- instance ToJSON Person
 
 instance ToJSON (Tagged V1 Person) where
     toJSON (Tagged (Person name _)) = object [ "name" .= name ]
@@ -90,3 +125,19 @@ instance FailableToJSON (Tagged V3 Person) where
     | otherwise = Just $ object [ "name" .= name
                                 , "age" .= age
                                 ]
+
+instance SerializedVersion Person where
+  type SerializerVersions Person = '[V1, V2, V3]
+
+instance FromJSON (Tagged V1 Person) where
+  parseJSON (Object o) = Tagged <$> (Person <$> o .: "name"
+                                            <*> o .: "age")
+  parseJSON _ = mempty
+
+instance FromJSON (Tagged V2 Person) where
+  parseJSON (Object o) = Tagged <$> (Person <$> o .: "name"
+                                            <*> pure 25)
+  parseJSON _ = mempty
+
+instance DeserializedVersion Person where
+  type DeserializerVersions Person = '[V1, V2]
